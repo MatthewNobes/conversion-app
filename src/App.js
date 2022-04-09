75b025850a8ea88df07ee2e1aaca120840e4d3ed
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-04-09 20:12:28 +0100

    #5 Nav list rework

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import Header from './Components/Header';
+import Header from "./Components/Header";
+import NavList from "./Components/NavList";
 
-import Home from './Components/Home'
-import DistanceConversion from './Components/Distance/DistanceConversion';
-import MassConversion from './Components/Mass/MassConversion';
-
-const Nav = () => {
-  return (
-    <nav>
-        <Router className="React-Router">
-          <div>
-            <ul className="React-Router-Group">
-              <li className="React-Router-Headlinks">
-                <Link className="React-Router-Links" to="/">Home</Link>
-              </li>
-              <li className="React-Router-Headlinks">
-                <Link className="React-Router-Links" to="/Mass">Mass</Link>
-              </li>
-              <li className="React-Router-Headlinks">
-                <Link className="React-Router-Links" to="/Distance">Distance</Link>
-              </li>
-            </ul>
-
-            <Routes>
-              <Route path="/Distance" element={<DistanceConversion/>} />
-              <Route path="/Mass" element={<MassConversion/>} />
-              <Route path="/" element={<Home/>} />
-            </Routes>
-          </div>
-        </Router>
-      </nav>
-  )
-}
+import Home from "./Components/Home";
+import DistanceConversion from "./Components/Distance/DistanceConversion";
+import MassConversion from "./Components/Mass/MassConversion";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Nav />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <NavList />
+        <Routes>
+          <Route path="/Distance" element={<DistanceConversion />} />
+          <Route path="/Mass" element={<MassConversion />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
