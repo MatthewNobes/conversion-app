7be66ac47435a105fecee1afbc9287ddd488dfa1
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-05-01 12:43:40 +0100

    #7 Fixing browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/system";
 import Menu from "./components/Menu";
 import Home from "./components/Home";
@@ -8,18 +8,16 @@ import DistanceConversion from "./features/Distance";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Menu />
-        <Box sx={{ paddingTop: "64px", paddingLeft: "64px" }}>
-          <Routes>
-            <Route path="/Distance" element={<DistanceConversion />} />
-            <Route path="/Mass" element={<MassConversion />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Box>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Menu />
+      <Box sx={{ paddingTop: "64px", paddingLeft: "64px" }}>
+        <Routes>
+          <Route path="/Distance" element={<DistanceConversion />} />
+          <Route path="/Mass" element={<MassConversion />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Box>
+    </div>
   );
 };
 
