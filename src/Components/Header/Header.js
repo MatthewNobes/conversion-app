965867ef4c4fe736f4ab80444595b08423b7d6b0
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-19 19:21:10 +0100

    Fixing variant mapping error

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 
 export const Header = () => (
-  <Typography variant="h4" variantMapping="h1">
+  <Typography variant="h4" component="h1">
     Conversion App
   </Typography>
 );
