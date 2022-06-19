04806b2e04e1b10fdf7695399993ac9822f70d6f
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-19 19:30:41 +0100

    #20 Swap button disabled on same units

--- src/features/ConversionForm/SwapButton/SwapButton.js
+++ src/features/ConversionForm/SwapButton/SwapButton.js
@@ -2,17 +2,25 @@ import { Button } from "@mui/material";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import { swapValues } from "./swapValues";
 
-export const SwapButton = (props) => (
-  <Button
-    startIcon={<SwapHorizIcon />}
-    onClick={() => {
-      const initialStartValue = props.startValue;
-      const initialEndValue = props.endValue;
-      swapValues(initialEndValue, "convertFrom", props.changeFn);
-      swapValues(initialStartValue, "convertTo", props.changeFn);
-      props.reCalculateFn();
-    }}
-  >
-    Swap
-  </Button>
-);
+export const SwapButton = (props) => {
+  const initialStartValue = props.startValue;
+  const initialEndValue = props.endValue;
+  let valuesEqual = false;
+  if (initialStartValue === initialEndValue) {
+    valuesEqual = true;
+  }
+
+  return (
+    <Button
+      startIcon={<SwapHorizIcon />}
+      disabled={valuesEqual}
+      onClick={() => {
+        swapValues(initialEndValue, "convertFrom", props.changeFn);
+        swapValues(initialStartValue, "convertTo", props.changeFn);
+        props.reCalculateFn();
+      }}
+    >
+      Swap
+    </Button>
+  );
+};
